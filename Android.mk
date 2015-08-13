aa7c9f376b5e8b37db1ef7dc3f860f0776a2613f
Alexander Martinz <eviscerationls@gmail.com>
2015-08-13 10:22:56 +0200

    set up for our needs
    Change-Id: I30fa2df68cad4b7e8c83facf2e5e098bda55c1e5
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,13 +1,14 @@
 ifneq (, $(filter aarch64 arm64, $(TARGET_ARCH)))
     $(info TODOAArch64: $(LOCAL_PATH)/Android.mk: Enable compile for 64 bit)
 else
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092 msm_bronze msm8916 plutonium,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7630_surf apq8084 mpq8092 msm_bronze msm8916 plutonium,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 endif
 
@@ -17,5 +18,6 @@ endif
 
 ifneq ($(filter apq8084,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
+endif
 endif
 endif # TARGET_ARCH
